87d2cb091c1678194da889fc4ac11fc8e6eda3c8
AdiFischer <adifischer1@gmail.com>
2022-10-20 16:35:16 -0400

    updeted an item

--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -1,16 +1,50 @@
 import dbConnect from "./dbConnect.js";
+import { ObjectId } from "mongodb";
 
 export async function getAllFurniture(req, res) {
     // connect to db
     const db = dbConnect()
 
-    //get the whole futniture collection
+    //get the whole furniture collection
     const collection = await db.collection("furniture").find().toArray()
-    //ctach err status 500
-    .catch(err => {
-        res.status(500).send(err);
-        return;
-    })
+        //catch err status 500
+        .catch(err => {
+            res.status(500).send(err);
+            return;
+        })
     //send back array furniture
     res.send(collection);
+}
+
+//create
+export async function addNewFurniture(req, res) {
+
+    //get new furniture from the body of the request
+    const { brand, modle, type, price } = req.body
+    const newFurniture = { brand, modle, type, price }// price: Number(price)-if price is num, not string
+    //connect db
+    const db = dbConnect()
+    //put the new furniture in our furn collection in our db
+    await db.collection('furniture').insertOne(newFurniture)
+        //catch err and send with status 500
+        .catch(err => {
+            res.status(500).send(err)
+            return
+        })
+    // return res with 201
+    res.status(201).send({ message: 'Furniture added ' })
+}
+// update
+export async function updateFurniture(req, res) {
+
+    const { furnitureId } = req.params
+    const db = dbConnect()
+
+    await db.collection('furniture')
+        .findOneAndUpdate({ _id: new ObjectId(furnitureId) }, { $set: req.body })
+        .catch(err => {
+            res.status(500).send(err)
+            return
+        })
+    res.status(202).send({ message: "furniture updated" })
 }
